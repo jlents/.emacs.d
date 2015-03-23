c8cb2088150f4f6616daf257d5c70695e345d58c
Joshua Lents <joshua.lents@gmail.com>
2015-03-23 10:28:53 -0400

    updating .emacs.d

--- clojure-stuff.el
+++ clojure-stuff.el
@@ -4,8 +4,8 @@
   (cljr-add-keybindings-with-prefix "C-c C-m"))
 
 ;; nREPL: The new Clojure network REPL. See https://github.com/kingtim/nrepl.el
-(require 'nrepl)
-(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+;(require 'nrepl)
+;(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
 (add-to-list 'same-window-buffer-names "<em>nrepl</em>")
 (setq nrepl-popup-stacktraces nil)
 (setq nrepl-log-messages t)
@@ -15,31 +15,30 @@
 ;; Autocompletion for nREPL; From https://github.com/purcell/ac-nrepl
 (require 'ac-nrepl)
 (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
-(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+;(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
 (add-hook 'cider-mode-hook 'ac-nrepl-setup)
 (add-hook 'cider-repl-mode-hook 'ac-nrepl-setup)
 
 ;;(add-to-list 'ac-modes 'cider-mode)
 ;;(add-to-list 'ac-modes 'cider-repl-mode)
 
-(defvar modes-ac '('(add-to-list 'ac-modes'nrepl-mode)))  
+;(defvar modes-ac '('(add-to-list 'ac-modes'nrepl-mode)))  
 ;                   '(add-to-list 'ac-modes 'cider-mode) 
 ;                   '(add-to-list 'ac-modes 'cider-repl-mode)))
-(dolist (m modes-ac)
-  (eval-after-load "auto-complete" m))
+;; (dolist (m modes-ac)
+;;   (eval-after-load "auto-complete" m))
+(eval-after-load "auto-complete"
+  '(add-to-list 'ac-modes'nrepl-mode))  
+(eval-after-load "cider"
+  '(define-key cider-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc))
 
 ;; Probably gets in the way of yasnippet?
 (defun set-auto-complete-as-completion-at-point-function ()
   (setq completion-at-point-functions '(auto-complete)))
+
 (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
-
 (add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
-(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
-
-
-
-;; Popup documentation change
-(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
+;(add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
 ;; Glue code: Bring it all together...
 (add-hook 'cider-mode-hook #'eldoc-mode)
@@ -54,8 +53,21 @@
 
 (setq cider-prefer-local-resources t)
 (setq cider-repl-pop-to-buffer-on-connect nil)
-(setq cider-repl-display-in-current-window t) ; C-c C-z (opens repl in window)
+;(setq cider-repl-display-in-current-window t) ; C-c C-z (opens repl in window)
 (add-hook 'cider-repl-mode-hook #'company-mode)
 (add-hook 'cider-mode-hook #'company-mode)
 (add-hook 'cider-repl-mode-hook #'subword-mode)
 (add-hook 'cider-repl-mode-hook #'paredit-mode)
+(setq cider-repl-pop-to-buffer-on-connect t)
+
+
+
+
+
+;; Popup documentation change
+;(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
+
+;; Show parenthesis mode
+(show-paren-mode 1)
+
+;; (global-rainbow-delimiters-mode)
